63d9101bb26f143c4634514bcb28b1700ed408d5
Anton Simola <anton.simola@gmail.com>
2021-08-08 14:11:24 +0300

    angular 9

--- projects/angular-highcharts-declarative-showcase/src/app/app.module.ts
+++ projects/angular-highcharts-declarative-showcase/src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatTableModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
